6750e63ae9d89b7d1fab2e9ec92c0dace0fdefa5
phuongpro123321 <77892242+phuongpro123321@users.noreply.github.com>
2021-04-01 16:19:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php	 
  $conn = pg_connect("host=ec2-18-206-20-102.compute-1.amazonaws.com dbname=dcdunsnprbc06j user=synxttjivonovb password=435e3c81b08bbf01bfbd5c1a498535976a24043baf1af6290f8b06137ef238b7 port=5432");
- if(!$conn){echo "Not Connect";}
+ if($conn){echo "Not Connect";}
  if(isset($_POST['login'])){
 	$username = $_POST['username'];
  	$password = $_POST['password'];
